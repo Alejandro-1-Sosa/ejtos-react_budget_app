8726e588e7b42b48b855964d6f55453bcf9a5d52
Alejandro-1-Sosa <141190092+Alejandro-1-Sosa@users.noreply.github.com>
2024-03-08 20:36:19 -0300

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -6,22 +6,33 @@ const Budget = () => {
   const [newBudget, setNewBudget] = useState(budget);
 
   const handleBudgetChange = (event) => {
-    setNewBudget(event.target.value);
+    // Get the new budget value
+    const newBudgetValue = parseInt(event.target.value, 10);
+
+    // Check if the new value exceeds the limit of 20,000
+    if (newBudgetValue > 20000) {
+      alert('The budget cannot exceed 20,000.');
+      return;
+    }
+
+    // Update the state with the new budget value
+    setNewBudget(newBudgetValue);
   }
 
   // Budget.js
-useEffect(() => {
-    dispatch({ type: 'UPDATE_BUDGET', payload: parseInt(newBudget, 10) });
+  useEffect(() => {
+    // Update the budget and remaining when the new budget changes
+    dispatch({ type: 'UPDATE_BUDGET', payload: newBudget });
     dispatch({ type: 'UPDATE_REMAINING' });
-}, [newBudget, dispatch]);
-
+  }, [newBudget, dispatch]);
 
   return (
     <div className='alert alert-secondary'>
       <span>Budget: £ {}</span>
-      <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+      <input type="number" step="10" max="20000" value={newBudget} onChange={handleBudgetChange}></input>
     </div>
   );
 };
 
 export default Budget;
+
