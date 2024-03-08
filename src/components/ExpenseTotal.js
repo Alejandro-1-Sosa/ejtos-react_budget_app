f28391649f1247e9274bdece34ee4077bd19e47d
Alejandro-1-Sosa <141190092+Alejandro-1-Sosa@users.noreply.github.com>
2024-03-08 19:40:51 -0300

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -3,16 +3,17 @@ import { AppContext } from '../context/AppContext';
 
 const ExpenseTotal = () => {
     const { expenses } = useContext(AppContext);
-    
+
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
-    
+
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+            <span>Spent so far: £ {totalExpenses}</span>
         </div>
     );
 };
+
 export default ExpenseTotal;
 
