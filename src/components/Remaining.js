1d913c8f388cfa23e8e42d08386713b0120d122e
Alejandro-1-Sosa <141190092+Alejandro-1-Sosa@users.noreply.github.com>
2024-03-08 08:58:35 -0300

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,32 +1,17 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-
 const Remaining = () => {
     const { expenses, budget } = useContext(AppContext);
-    const [remaining, setRemaining] = useState(budget - calculateTotalExpenses(expenses));
-
-    useEffect(() => {
-        // Update remaining whenever the budget or expenses change
-        setRemaining(budget - calculateTotalExpenses(expenses));
-    }, [budget, expenses]);
-
-    // Determine the style class based on whether the total expenses exceed the budget
-    const alertType = remaining < 0 ? 'alert-danger' : 'alert-success';
-
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
     return (
         <div className={`alert ${alertType}`}>
-            <span>Remaining: £{remaining}</span>
+            <span>Remaining: £{budget - totalExpenses}</span>
         </div>
     );
 };
-
-// Helper function to calculate total expenses
-const calculateTotalExpenses = (expenses) => {
-    return expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
-};
-
 export default Remaining;
 
 
